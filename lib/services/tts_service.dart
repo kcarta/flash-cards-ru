308c3707a4ffdc965cff9a80dd5311a1e5d7c92e
Kristofer Carta <krc@trifork.com>
2024-03-29 09:15:56 +0100

    Fix voices

--- lib/services/tts_service.dart
+++ lib/services/tts_service.dart
@@ -1,12 +1,25 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
 class TTSService {
+  FlutterTts flutterTts = FlutterTts();
+
   Future<void> speak(String text) async {
-    FlutterTts flutterTts = FlutterTts();
-    await flutterTts.setVoice({"name": "Milena", "locale": "ru-RU"});
-    await flutterTts.setLanguage("ru-RU");
-    await flutterTts.setPitch(1.0);
-    await flutterTts.setSpeechRate(0.4);
+    await setupSpeech(); // TODO move this to a one-time init step
     await flutterTts.speak(text);
+  }
+
+  Future<void> setupSpeech() async {
+    await flutterTts.setLanguage("ru-RU");
+    var voices = await flutterTts.getVoices;
+    if (voices.any((voice) => voice["name"] == "Yuri (Enhanced)")) {
+      await flutterTts.setVoice({"name": "Yuri (Enhanced)", "locale": "ru-RU"});
+    } else if (voices.any((voice) => voice["name"] == "Milena (Enhanced)")) {
+      await flutterTts
+          .setVoice({"name": "Milena (Enhanced)", "locale": "ru-RU"});
+    } else {
+      await flutterTts.setVoice({"name": "Milena", "locale": "ru-RU"});
+    }
+    await flutterTts.setPitch(1.0);
+    await flutterTts.setSpeechRate(0.5);
   }
 }
